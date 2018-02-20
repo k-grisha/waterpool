5e2e8f9252cb3c731705634bfa5fe0bbb4547867
Ryhor <grisha.k@bk.ru>
2018-02-21 01:12:13 +0300

    solution 1

--- src/main/java/gr/proga/waterpool/WaterCollector.java
+++ src/main/java/gr/proga/waterpool/WaterCollector.java
@@ -1,17 +1,48 @@
 package gr.proga.waterpool;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WaterCollector {
-
-    public static int handle(int[] hills) {
-        return handle(Arrays.stream(hills).boxed().collect(Collectors.toList()));
-    }
+    private static final Integer MAX_SIZE = 32000;
 
     public static int handle(List<Integer> hills) {
+        Integer maxHill = getMaxHillAndValid(hills);
+        if (maxHill == 0) {
+            return 0;
+        }
+        Integer collector = 0;
 
-        return hills.size();
+        for (int h = maxHill; h >= 0; h--) {
+            List<Integer> hillsSection = new ArrayList<>();
+            for (int i = 0; i < hills.size(); i++) {
+                if (hills.get(i) >= h) {
+                    hillsSection.add(i);
+                }
+            }
+
+            for (int j = hillsSection.size() - 1; j > 0; j--) {
+                collector += hillsSection.get(j) - hillsSection.get(j - 1) - 1;
+            }
+        }
+
+        return collector;
     }
+
+    private static Integer getMaxHillAndValid(List<Integer> hills) {
+        if (hills.size() > MAX_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if (hills.size() <= 2) {
+            return 0;
+        }
+        Integer maxHill = hills.stream().max(Comparator.naturalOrder()).orElse(0);
+        if (maxHill > MAX_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        return maxHill;
+    }
+
+
 }
--- src/test/java/gr/proga/waterpool/WaterCollectorTest.java
+++ src/test/java/gr/proga/waterpool/WaterCollectorTest.java
@@ -1,15 +1,30 @@
 package gr.proga.waterpool;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static gr.proga.waterpool.WaterCollector.handle;
 
 public class WaterCollectorTest {
 
-    @Test
-    public void handleTest() {
-        System.out.println(handle(new int[]{1, 2, 3}));
+    @Test(expected = IllegalArgumentException.class)
+    public void handle_exception() {
+        System.out.println(handle(Arrays.asList(5, 2, 3, 4, 5, 32001, 1, 3, 1)));
     }
 
+    @Test
+    public void handle_zero() {
+        Assert.assertEquals(0, handle(Arrays.asList(5, 2)));
+        Assert.assertEquals(0, handle(Arrays.asList(0, 0, 0, 0)));
+    }
+
+    @Test
+    public void handle_success() {
+        Assert.assertEquals(8, handle(Arrays.asList(5, 2, 3, 4, 5, 4, 1, 3, 1)));
+        Assert.assertEquals(0, handle(Arrays.asList(1, 5, 8, 900, 99, 8, 6, 0)));
+        System.out.println( handle(Arrays.asList(1, 0, 1, 0, 32000)));
+    }
 
 }
