eb7770a979fd5f624d68474cfc6a03db935beddb
Ryhor <grisha.k@bk.ru>
2018-02-22 22:36:18 +0300

    code optimize

--- src/main/java/gr/proga/waterpool/WaterCollector.java
+++ src/main/java/gr/proga/waterpool/WaterCollector.java
@@ -1,60 +1,29 @@
 package gr.proga.waterpool;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class WaterCollector {
-	private static final Integer MAX_SIZE = 32000;
+	public static final Integer MAX_SIZE = 32000;
 
 	public static int handle(List<Integer> hills) {
-		Integer maxHill = getMaxHillAndValid(hills);
-		if (maxHill == 0) {
-			return 0;
-		}
-		Integer minHill = getMinHillAndValid(hills);
-		if (minHill.equals(maxHill)) {
-			return 0;
-		}
-
 		Integer collector = 0;
 
-		for (int h = maxHill; h >= minHill; h--) {
-			List<Integer> hillsSection = new ArrayList<>();
+		for (int h = 0; h <= MAX_SIZE; h++) {
+			List<Integer> hillsSection = new ArrayList<>(hills.size());
 			for (int i = 0; i < hills.size(); i++) {
 				if (hills.get(i) >= h) {
 					hillsSection.add(i);
 				}
 			}
+			if(hillsSection.size() == 0){
+				break;
+			}
 			for (int j = hillsSection.size() - 1; j > 0; j--) {
 				collector += hillsSection.get(j) - hillsSection.get(j - 1) - 1;
 			}
 		}
-
 		return collector;
-	}
-
-	private static Integer getMaxHillAndValid(List<Integer> hills) {
-		if (hills.size() > MAX_SIZE) {
-			throw new IllegalArgumentException("Landscape too big. Max = " + MAX_SIZE);
-		}
-		if (hills.size() <= 2) {
-			return 0;
-		}
-		Integer maxHill = hills.stream().max(Comparator.naturalOrder()).orElse(0);
-		if (maxHill > MAX_SIZE) {
-			throw new IllegalArgumentException("Hill too high. Max = " + MAX_SIZE);
-		}
-
-		return maxHill;
-	}
-
-	private static Integer getMinHillAndValid(List<Integer> hills) {
-		Integer minHill = hills.stream().min(Comparator.naturalOrder()).orElse(0);
-		if (minHill < 0) {
-			throw new IllegalArgumentException("Pits too low");
-		}
-		return minHill;
 	}
 
 
--- src/test/java/gr/proga/waterpool/WaterCollectorTest.java
+++ src/test/java/gr/proga/waterpool/WaterCollectorTest.java
@@ -1,31 +1,16 @@
 package gr.proga.waterpool;
 
+import com.google.common.base.Stopwatch;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
+import static gr.proga.waterpool.WaterCollector.MAX_SIZE;
 import static gr.proga.waterpool.WaterCollector.handle;
 
 public class WaterCollectorTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void handle_tooHigh_exception() {
-        System.out.println(handle(Arrays.asList(5, 2, 3, 4, 5, 32001, 1, 3, 1)));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void handle_tooLow_exception() {
-        System.out.println(handle(Arrays.asList(5, 2, 3, 4, 5, -1, 1, 3, 1)));
-    }
-
-    @Test
-    public void handle_zero() {
-        Assert.assertEquals(0, handle(Collections.emptyList()));
-        Assert.assertEquals(0, handle(Arrays.asList(5, 2)));
-        Assert.assertEquals(0, handle(Arrays.asList(0, 0, 0, 0)));
-    }
 
     @Test
     public void handle_success() {
@@ -36,6 +21,26 @@ public class WaterCollectorTest {
         Assert.assertEquals(99, handle(Arrays.asList(100, 1, 100)));
         Assert.assertEquals(1, handle(Arrays.asList(3000, 3000, 2999, 3000, 3000)));
         Assert.assertEquals(0, handle(Arrays.asList(3000, 3000, 3000, 3000, 1)));
+        Assert.assertEquals(0, handle(Collections.emptyList()));
+        Assert.assertEquals(0, handle(Arrays.asList(5, 2)));
+        Assert.assertEquals(0, handle(Arrays.asList(0, 0, 0, 0)));
+    }
+
+
+    @Test
+    public void performance_test() {
+        Random rnd = new Random();
+        List<Integer> hills = new ArrayList<>(MAX_SIZE);
+        for (int i = 0; i < 10; i++) {
+            hills.add(rnd.nextInt(100));
+        }
+
+        Stopwatch stopwatch1 = Stopwatch.createStarted();
+        int res1 = handle(hills);
+        stopwatch1.stop();
+
+        System.out.println("res= " + res1 + " t1= " + stopwatch1 );
+
     }
 
 }
