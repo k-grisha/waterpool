0d0feac7f248b6a483e4c030c4f7d19ca19b0e69
Ryhor <grisha.k@bk.ru>
2018-02-23 08:43:07 +0300

    refactoring

--- src/main/java/gr/proga/waterpool/WaterCollectorImpl2.java
+++ src/main/java/gr/proga/waterpool/WaterCollectorImpl2.java
@@ -6,61 +6,60 @@ import java.util.Map;
 
 public class WaterCollectorImpl2 implements WaterCollector {
 
-    @Override
-    public int handle(List<Integer> hills) {
-        Streams streams = new Streams();
-        Integer previousHill = 0;
-        Integer peak = 0;
-        int collector = 0;
-        for (Integer hill : hills) {
-            if (hill >= peak) {
-                peak = hill;
-                collector += streams.collectAll();
-            }else if (hill <= previousHill) {
-                for (Integer i = hill + 1; i <= peak; i++) {
-                    streams.increase(i);
-                }
-            }
-            else {
-                for (Integer i = previousHill + 1; i <= hill; i++) {
-                    collector += streams.collect(i);
-                }
-                for (Integer i = hill + 1; i <= peak; i++) {
-                    streams.increase(i);
-                }
-            }
-            previousHill = hill;
-        }
-        return collector;
-    }
+	@Override
+	public int handle(List<Integer> hills) {
+		Liquid liquid = new Liquid();
+		Integer previousHill = 0;
+		Integer peak = 0;
+		int collector = 0;
+		for (Integer hill : hills) {
+			if (hill >= peak) {
+				peak = hill;
+				collector += liquid.collectAll();
+			} else if (hill <= previousHill) {
+				for (Integer i = hill + 1; i <= peak; i++) {
+					liquid.increase(i);
+				}
+			} else {
+				for (Integer i = previousHill + 1; i <= hill; i++) {
+					collector += liquid.collect(i);
+				}
+				for (Integer i = hill + 1; i <= peak; i++) {
+					liquid.increase(i);
+				}
+			}
+			previousHill = hill;
+		}
+		return collector;
+	}
 
 
-    class Streams {
-        Map<Integer, Integer> streams = new HashMap<>();
+	class Liquid {
+		Map<Integer, Integer> streams = new HashMap<>();
 
-        public void increase(Integer i) {
-            Integer stream = streams.get(i);
-            if (stream == null) {
-                streams.put(i, 1);
-            } else {
-                streams.put(i, ++stream);
-            }
-        }
+		void increase(Integer i) {
+			Integer stream = streams.get(i);
+			if (stream == null) {
+				streams.put(i, 1);
+			} else {
+				streams.put(i, ++stream);
+			}
+		}
 
-        public int collectAll() {
-            int res = 0;
-            for (Integer val : streams.values()) {
-                res += val;
-            }
-            streams.clear();
-            return res;
-        }
+		int collectAll() {
+			int res = 0;
+			for (Integer val : streams.values()) {
+				res += val;
+			}
+			streams.clear();
+			return res;
+		}
 
-        public int collect(Integer i) {
-            int res = streams.get(i);
-            streams.remove(i);
-            return res;
-        }
-    }
+		int collect(Integer i) {
+			int res = streams.get(i);
+			streams.remove(i);
+			return res;
+		}
+	}
 
 }
--- src/test/java/gr/proga/waterpool/WaterCollectorTest.java
+++ src/test/java/gr/proga/waterpool/WaterCollectorTest.java
@@ -11,19 +11,8 @@ public class WaterCollectorTest {
     private WaterCollector wc1 = new WaterCollectorImpl1();
     private WaterCollector wc2 = new WaterCollectorImpl2();
 
-    @Test(expected = IllegalArgumentException.class)
-    public void handle_tooHigh_exception() {
-        System.out.println(wc1.handle(Arrays.asList(5, 2, 3, 4, 5, 32001, 1, 3, 1)));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void handle_tooLow_exception() {
-        System.out.println(wc1.handle(Arrays.asList(5, 2, 3, 4, 5, -1, 1, 3, 1)));
-    }
-
     @Test
     public void handle_success() {
-
         Stopwatch stopwatch1 = Stopwatch.createStarted();
         handle_test(wc1);
         stopwatch1.stop();
@@ -33,29 +22,6 @@ public class WaterCollectorTest {
         stopwatch2.stop();
 
         System.out.println(" t1= " + stopwatch1 + " t2= " + stopwatch2);
-
-    }
-
-    private void handle_test(WaterCollector waterCollector) {
-        for (int i = 0; i < 100; i++) {
-            Assert.assertEquals(0, waterCollector.handle(Collections.emptyList()));
-            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(5, 2)));
-            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(0, 0, 0, 0)));
-            Assert.assertEquals(8, waterCollector.handle(Arrays.asList(5, 2, 3, 4, 5, 4, 1, 3, 1)));
-            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(5, 5, 5, 5)));
-            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(1, 5, 8, 900, 99, 8, 6, 0)));
-            Assert.assertEquals(2, waterCollector.handle(Arrays.asList(1, 0, 1, 0, 32000)));
-            Assert.assertEquals(99, waterCollector.handle(Arrays.asList(100, 1, 100)));
-            Assert.assertEquals(1, waterCollector.handle(Arrays.asList(100, 1, 2)));
-            Assert.assertEquals(1, waterCollector.handle(Arrays.asList(3000, 3000, 2999, 3000, 3000)));
-            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(3000, 3000, 3000, 3000, 1)));
-        }
-    }
-
-    @Test
-    public void bugFix() {
-        Assert.assertEquals(8, wc2.handle(Arrays.asList(6, 0, 2, 5)));
-        Assert.assertEquals(8, wc1.handle(Arrays.asList(6, 0, 2, 5)));
     }
 
     @Test
@@ -73,9 +39,25 @@ public class WaterCollectorTest {
         Stopwatch stopwatch2 = Stopwatch.createStarted();
         int res2 = wc2.handle(hills);
         stopwatch2.stop();
+
         Assert.assertEquals(res1, res2);
-
         System.out.println("res= " + res1 + " t1= " + stopwatch1 + " t2= " + stopwatch2);
+    }
 
+    private void handle_test(WaterCollector waterCollector) {
+        for (int i = 0; i < 1000; i++) {
+            Assert.assertEquals(0, waterCollector.handle(Collections.emptyList()));
+            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(5, 2)));
+            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(0, 0, 0, 0)));
+            Assert.assertEquals(8, waterCollector.handle(Arrays.asList(5, 2, 3, 4, 5, 4, 1, 3, 1)));
+            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(5, 5, 5, 5)));
+            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(1, 5, 8, 900, 99, 8, 6, 0)));
+            Assert.assertEquals(2, waterCollector.handle(Arrays.asList(1, 0, 1, 0, 32000)));
+            Assert.assertEquals(99, waterCollector.handle(Arrays.asList(100, 1, 100)));
+            Assert.assertEquals(1, waterCollector.handle(Arrays.asList(100, 1, 2)));
+            Assert.assertEquals(1, waterCollector.handle(Arrays.asList(3000, 3000, 2999, 3000, 3000)));
+            Assert.assertEquals(0, waterCollector.handle(Arrays.asList(3000, 3000, 3000, 3000, 1)));
+            Assert.assertEquals(8, waterCollector.handle(Arrays.asList(6, 0, 2, 5)));
+        }
     }
 }
